1739236dc6c2391ab90f6e3d08ab6aa0a7e315cc
hejian <hejian@wsl>
2021-02-27 15:32:00 +0800

    fix bugs - cvtk/models/segmentation/coco_utils.py

--- cvtk/models/segmentation/coco_utils.py
+++ cvtk/models/segmentation/coco_utils.py
@@ -8,7 +8,7 @@ from cvtk.io import load_json
 from torchvision.transforms import functional as F
 
 
-def check_bboxes(src_bboxes, dst_bboxes):
+def check_bboxes(src_bboxes, dst_bboxes, nonignore):
     src_w = src_bboxes[:, 2] - src_bboxes[:, 0]
     src_h = src_bboxes[:, 3] - src_bboxes[:, 1]
     src_area = src_w * src_h
@@ -19,10 +19,11 @@ def check_bboxes(src_bboxes, dst_bboxes):
 
     x = np.where(src_area > 64**2, 0.5, 1.0 - 1e-5)
 
-    s1 = (dst_area >= src_area * x)
-    s2 = (dst_w >= src_w) * (dst_h >= src_w * 2)
-    s3 = (dst_h >= src_h) * (dst_w >= src_h * 2)
-    ss = s1 + s2 + s3
+    s1 = (dst_area >= nonignore)
+    s2 = (dst_area >= src_area * x)
+    s3 = (dst_w >= src_w) * (dst_h >= src_w * 2)
+    s4 = (dst_h >= src_h) * (dst_w >= src_h * 2)
+    ss = s1 + s2 + s3 + s4
 
     inner = (dst_w >= 4) * (dst_h >= 4)
     return ss * inner, np.logical_not(ss) * inner
@@ -35,6 +36,7 @@ class ToyDataset:
         self._img_prefix = Path(data_root)
         self._single_cls = single_cls
         self._crop_size = crop_size
+        self._safe = crop_size**2
         self._phase = phase
 
         if phase == "train":
@@ -70,6 +72,7 @@ class ToyDataset:
             bbox = [x, y, x + w, y + h]
             label = id2label[ann["category_id"]]
             imdb[ann["image_id"]].append((bbox, label))
+        imdb = {k: list(map(list, zip(*v))) for k, v in imdb.items()}
 
         self.imgs = imgs
         self.imdb = imdb
@@ -80,6 +83,13 @@ class ToyDataset:
         img_h, img_w = image.shape[:2]
 
         if self._crop_size < min(img_h, img_w):
+
+            if len(bboxes) == 0:
+                x1 = np.random.randint(0, img_w - self._crop_size)
+                y1 = np.random.randint(0, img_h - self._crop_size)
+                x2, y2 = x1 + self._crop_size, y1 + self._crop_size
+                return image[y1:y2, x1:x2], bboxes, labels
+
             bboxes = np.asarray(bboxes, dtype=np.int64)
             labels = np.asarray(labels, dtype=np.int64)
             old_bboxes = bboxes.copy()
@@ -87,36 +97,29 @@ class ToyDataset:
             n = bboxes.shape[0]
             ind = np.random.randint(n)
             x1, y1, x2, y2 = bboxes[ind].tolist()
-
             if x2 - x1 < self._crop_size:
                 x1 = max(0, np.random.randint(x2 - self._crop_size, x1))
             if y2 - y1 < self._crop_size:
                 y1 = max(0, np.random.randint(y2 - self._crop_size, y1))
-
             x2, y2 = x1 + self._crop_size, y1 + self._crop_size
 
             if x2 > img_w:
                 offset = x2 - img_w
                 x1, x2 = x1 - offset, x2 - offset
-
             if y2 > img_h:
                 offset = y2 - img_h
                 y1, y2 = y1 - offset, y2 - offset
 
-            image = image[y1: y2, x1: x2]
-
             bboxes[:, 0::2] = np.clip(bboxes[:, 0::2], x1, x2)
             bboxes[:, 1::2] = np.clip(bboxes[:, 1::2], y1, y2)
             bboxes -= np.asarray([x1, y1, x1, y1])
+            image = image[y1: y2, x1: x2]
 
-            dst_mask, drop_mask = check_bboxes(old_bboxes, bboxes)
-
+            dst_mask, drop_mask = check_bboxes(old_bboxes, bboxes, self._safe)
             for x1, y1, x2, y2 in bboxes[drop_mask].tolist():
                 image[y1: y2, x1: x2] = 0
-
             bboxes = bboxes[dst_mask].tolist()
             labels = labels[dst_mask].tolist()
-            return image, bboxes, labels
 
         return image, bboxes, labels
 
@@ -128,7 +131,12 @@ class ToyDataset:
 
         image = cv.imread(str(image_path), 1)
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        bboxes, labels = map(list, zip(*self.imdb[image_id]))
+
+        tmp = self.imdb.get(image_id, None)
+        if tmp is None:
+            bboxes, labels = [], []
+        else:
+            bboxes, labels = tmp
 
         if self._phase == "train":
             image, bboxes, labels = self.random_crop(image, bboxes, labels)
