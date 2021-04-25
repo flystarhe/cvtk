c4815cc1019a93e96a8bd3b4e30f04d2e68323f7
hejian.host.mac <flystarhe@qq.com>
2021-04-25 12:05:03 +0800

    post cvtk/losses/train_segmentation_with_bbox/{free_loss|grid_loss|line_loss|topk_loss}.py

---
+++ cvtk/losses/train_segmentation_with_bbox/free_loss.py
@@ -0,0 +1,4 @@
+# idea
+# set gt-box is fg-pixel
+# select bg-pixel 3x fg-pixel
+# noise label free competition, guided by weights
---
+++ cvtk/losses/train_segmentation_with_bbox/topk_loss.py
@@ -0,0 +1,78 @@
+import torch
+from torch.nn import functional as F
+
+from ._utils import _balance, _mask_top_by_full, _rescale, _sorted_and_cat
+
+
+def make_target(s, feats, bboxes, labels=None, topk=3, balance=False, use_sigmoid=False):
+    """Assume `cross_entropy(ignore_index=-100)`.
+
+    Args:
+        feats (Tensor[K, H, W]): the first is background.
+        bboxes (List[List[int]]): such as `[[x1, y1, x2, y2],]`.
+        labels (List[int], optional): where each value in `[1, K-1]`.
+    """
+    if labels is None:
+        labels = [1] * len(bboxes)
+
+    if use_sigmoid:
+        feats = torch.sigmoid(feats)
+    else:
+        feats = F.softmax(feats, dim=0)
+
+    target = torch.zeros_like(feats[0], dtype=torch.int64)
+    for x1, y1, x2, y2, label in _sorted_and_cat(bboxes, labels):
+        x1, y1, x2, y2 = _rescale(x1, y1, x2, y2, s)
+        roi = feats[label, y1:y2, x1:x2]
+        assert roi.ndim == 2
+
+        if roi.numel() == 0:
+            continue
+
+        sub_target = torch.full_like(roi, -100, dtype=torch.int64)
+        k = topk if topk is not None else max(roi.shape)
+        selected = _mask_top_by_full(roi, k)
+        sub_target[selected] = label
+
+        target[y1:y2, x1:x2] = sub_target
+
+    if balance:
+        target = _balance(target, feats[0])
+
+    return target
+
+
+def transform(pred, target, topk=3, balance=True, use_sigmoid=False):
+    # `pred (Tensor[N, C, H, W])` and `target (List[Dict])`.
+    s = pred.shape[-1] / target[0]["img_shape"][-1]
+    pred = pred.detach()
+
+    _target = []
+    for i in range(pred.shape[0]):
+        _target.append(make_target(
+            s, pred[i], target[i]["bboxes"], target[i]["labels"], topk, balance, use_sigmoid))
+    return torch.stack(_target, 0)
+
+
+def criterion(inputs, target, topk=3, balance=True, use_sigmoid=False):
+    """
+    Args:
+        inputs (OrderedDict): required `out`, optional `aux`.
+        target (List[Dict]): required `bboxes`, `img_shape`, `labels`.
+    """
+    _pred = inputs["out"]
+    _target = transform(_pred, target, topk, balance, use_sigmoid)
+    if use_sigmoid:
+        raise NotImplementedError("Not Implemented ...")
+    else:
+        loss = F.cross_entropy(_pred, _target, ignore_index=-100)
+
+    if "aux" in inputs:
+        _pred = inputs["aux"]
+        _target = transform(_pred, target, topk, balance, use_sigmoid)
+        if use_sigmoid:
+            raise NotImplementedError("Not Implemented ...")
+        else:
+            return loss + 0.5 * F.cross_entropy(_pred, _target, ignore_index=-100)
+
+    return loss
