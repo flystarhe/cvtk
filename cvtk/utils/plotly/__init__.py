9c0442586c510ac86fc092fb591224bc462306c7
hejian.host.mac <flystarhe@qq.com>
2021-05-12 16:56:34 +0800

    fix cvtk/utils/plotly/__init__.py

--- cvtk/utils/plotly/__init__.py
+++ cvtk/utils/plotly/__init__.py
@@ -1,3 +1,4 @@
-from .image import imhist
+from .color import color_hist
+from .image import image_hist
 
-__all__ = ["imhist"]
+__all__ = ["color_hist", "image_hist"]
--- cvtk/utils/plotly/color.py
+++ cvtk/utils/plotly/color.py
@@ -9,11 +9,10 @@ IMG_EXTENSIONS = set([".jpg", ".jpeg", ".png", ".bmp"])
 
 def _hist(npimg):
     hist = np.bincount(npimg.ravel(), minlength=256)
-    v_sum, h_sum = np.sum(npimg, axis=1), np.sum(npimg, axis=0)
-    return hist, v_sum, h_sum
+    return hist
 
 
-def image_hist(img_path, start=0, limit=1):
+def color_hist(img_path, start=0, limit=1):
     # jupyter: %matplotlib inline
     img_path = Path(img_path)
 
@@ -27,18 +26,22 @@ def image_hist(img_path, start=0, limit=1):
 
     fig, axs = plt.subplots(nrows=3, figsize=(8, 12))
     for img_path in img_list[start: start + limit]:
-        npimg = cv.imread(str(img_path), 0)
-        hist, v_sum, h_sum = _hist(npimg)
+        nparr = cv.imread(str(img_path), 1)
 
         ax = axs[0]
+        hist = _hist(nparr[..., 0])
         ax.plot(hist / (hist.max() + 1))
-        ax.set_title("Image grayscale value")
+        ax.set_title("Color [B] value")
         ax.set_xlim(0, 256)
 
         ax = axs[1]
-        ax.plot(v_sum / (v_sum.max() + 1))
-        ax.set_title("Vertical projection")
+        hist = _hist(nparr[..., 1])
+        ax.plot(hist / (hist.max() + 1))
+        ax.set_title("Color [G] value")
+        ax.set_xlim(0, 256)
 
         ax = axs[2]
-        ax.plot(h_sum / (h_sum.max() + 1))
-        ax.set_title("Horizontal projection")
+        hist = _hist(nparr[..., 2])
+        ax.plot(hist / (hist.max() + 1))
+        ax.set_title("Color [R] value")
+        ax.set_xlim(0, 256)
