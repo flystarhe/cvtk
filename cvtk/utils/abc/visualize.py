7e86bba8d4bf377b0d4faffaf05b79542aeb82b8
hejian.host.mac <flystarhe@qq.com>
2021-05-15 12:08:52 +0800

    fix cvtk/utils/abc/visualize.py

--- cvtk/utils/abc/visualize.py
+++ cvtk/utils/abc/visualize.py
@@ -22,17 +22,17 @@ def draw_bbox(img, anns, offset=0, color_val=(0, 0, 255)):
 
     img_h, img_w = img.shape[:2]
     for i, ann in enumerate(anns, 1):
-        x, y, w, h = map(int, ann["bbox"])
+        x, y, w, h = list(map(int, ann["bbox"]))
         cv.rectangle(img, (x, y), (x + w, y + h), color_val, thickness=2)
 
-        if y >= 120:
+        if y >= 200:
             left_bottom = (x, y - offset)
         elif h >= img_h * 0.5:
             left_bottom = (x, y + h - offset)
         else:
             left_bottom = (x, y + h + 30 + offset)
 
-        text = "{}: {}: {:.2f}: {}/{}={:.2f}".format(
+        text = "{} {} {:.2f} {}/{}={:.2f}".format(
             i, ann["label"], ann.get("score", 1.0), h, w, h / w)
         cv.putText(img, text, left_bottom,
                    cv.FONT_HERSHEY_COMPLEX, 1.0, color_val)
@@ -77,7 +77,7 @@ def display_coco(coco_dir, coco_file, output_dir, **kw):
         cv.putText(img, text, (15, 30),
                    cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 255))
 
-        img = draw_bbox(img, gts, offset=0, color_val=(0, 255, 0))
+        img = draw_bbox(img, gts, offset=5, color_val=(0, 255, 0))
         cv.imwrite(str(output_dir / file_name.name), img)
     return str(output_dir)
 
@@ -126,7 +126,7 @@ def display_test(results, score_thr, output_dir, **kw):
             cv.putText(img, text, (15, 60 + 30 * i),
                        cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 255))
 
-        img = draw_bbox(img, dts, offset=0, color_val=(0, 0, 255))
-        img = draw_bbox(img, gts, offset=30, color_val=(0, 255, 0))
+        img = draw_bbox(img, dts, offset=5, color_val=(0, 0, 255))
+        img = draw_bbox(img, gts, offset=35, color_val=(0, 255, 0))
         cv.imwrite(str(output_dir / file_name.name), img)
     return str(output_dir)
