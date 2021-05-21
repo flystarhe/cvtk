46ed2e8a692936357319c338279009a1cd98d524
hejian.host.mac <flystarhe@qq.com>
2021-05-21 17:37:34 +0800

    fine references/matching_pcb/visualize.py

--- references/matching_pcb/visualize.py
+++ references/matching_pcb/visualize.py
@@ -4,34 +4,37 @@ from skimage.color import label2rgb
 from skimage.morphology import diamond
 
 
-def label_regions(image, mask, color=(255, 0, 0)):
-    mask = (mask > 0).astype(np.uint8)
-
-    # kernel = np.ones((3, 3), np.uint8)
-    mask = cv.dilate(mask, diamond(2)) - mask
-
-    area = np.stack([mask * c for c in color], axis=2)
-
-    mask_inv = cv.bitwise_not(mask)
-    img1 = cv.bitwise_and(area, area, mask=mask)
-    img2 = cv.bitwise_and(image, image, mask=mask_inv)
-
-    dst = cv.add(img1, img2)
-    return dst
+def _color(mask, color):
+    return np.stack([mask * c for c in color], axis=2)
 
 
-def label_regions2(image, mask, color=(255, 0, 0)):
-    mask = (mask > 0).astype(np.uint8)
+def label_regions(image, masks, colors=(255, 0, 0), only_edge=True):
+    if not isinstance(masks, list):
+        masks = [masks]
 
-    area = np.stack([mask * c for c in color], axis=2)
+    if not isinstance(colors, list):
+        colors = [colors]
 
-    mask_inv = cv.bitwise_not(mask)
-    img1 = cv.bitwise_and(area, area, mask=mask)
-    img2 = cv.bitwise_and(image, image, mask=mask_inv)
+    masks = [(m > 0).astype(np.uint8) for m in masks]
 
-    # cv.addWeighted(image, 0.7, dst, 0.3, 0)
-    dst = cv.add(img1, img2)
-    return dst
+    if only_edge:
+        kernel = diamond(2)
+        # kernel = np.ones((3, 3), np.uint8)
+        masks = [cv.dilate(m, kernel) - m for m in masks]
+
+    _mask = np.stack(masks, axis=2).sum(axis=2)
+    _mask = (_mask > 0).astype(np.uint8)
+
+    area = np.zeros_like(image, dtype=np.int32)
+    for m, c in zip(masks, colors):
+        area += _color(m, c)
+    area = np.clip(area, 0, 255).astype(np.uint8)
+
+    _mask = np.stack((_mask, _mask, _mask), axis=2)
+    img1 = image * (1 - _mask)
+    img2 = area * _mask
+
+    return img1 + img2
 
 
 def label_to_rgb(label, image, color="red", alpha=0.3):
