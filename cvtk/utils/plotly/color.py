de8ee43eb16a26d14a4d486265b059d0c1ed7cbe
hejian.host.mac <flystarhe@qq.com>
2021-05-21 23:14:52 +0800

    update cvtk/utils/plotly/color.py

--- cvtk/utils/plotly/color.py
+++ cvtk/utils/plotly/color.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import cv2 as cv
+import hiplot as hip
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -29,11 +30,14 @@ def _hist2d_matrix(nparr, a, b, n=256):
     return matrix, x1, y1, x2, y2
 
 
-def _hist2d_dataframe(nparr, a, b, n=256):
+def _hist2d_dataframe(nparr, a, b, n=256, labels=None):
     matrix, x1, y1, x2, y2 = _hist2d_matrix(nparr, a, b, n)
 
-    rows = pd.Index(np.arange(y1, y2), name=f"c{a}")
-    cols = pd.Index(np.arange(x1, x2), name=f"c{b}")
+    if labels is None:
+        labels = [f"C{i}" for i in range(a + b + 1)]
+
+    rows = pd.Index(np.arange(y1, y2), name=labels[a])
+    cols = pd.Index(np.arange(x1, x2), name=labels[b])
     return pd.DataFrame(matrix, index=rows, columns=cols)
 
 
@@ -56,21 +60,24 @@ def color_hist(img_path, start=0, limit=1, mode="BGR"):
         nparr = cv.imread(str(img_path), 1)
 
         if mode == "BGR":
-            mode = ["B", "G", "R"]
+            labels = ["B", "G", "R"]
         elif mode == "HSV":
-            mode = ["H", "S", "V"]
+            labels = ["H", "S", "V"]
             nparr = cv.cvtColor(nparr, cv.COLOR_BGR2HSV)
         elif mode == "YCrCb":
-            mode = ["Y", "Cr", "Cb"]
+            labels = ["Y", "Cr", "Cb"]
             nparr = cv.cvtColor(nparr, cv.COLOR_BGR2YCrCb)
         else:
             raise Exception(f"{mode} not supported")
 
         for i in (0, 1, 2):
             hist = _hist(nparr[..., i])
-            axs[i].plot(hist / (hist.max() + 1))
-            axs[i].set_title(f"Color [{mode[i]}] value")
+            axs[i].plot(hist / hist.max(), label="count")
+            hist = np.cumsum(hist)
+            axs[i].plot(hist / hist.max(), label="cumsum")
+            axs[i].set_title(f"{labels[i]} of {mode}")
             axs[i].set_xlim(0, 256)
+            axs[i].legend()
 
 
 def color_hist2d(img_path, start=0, limit=1, mode="BGR"):
@@ -91,17 +98,54 @@ def color_hist2d(img_path, start=0, limit=1, mode="BGR"):
     nparr = cv.imread(str(img_path), 1)
 
     if mode == "BGR":
-        mode = ["B", "G", "R"]
+        labels = ["B", "G", "R"]
     elif mode == "HSV":
-        mode = ["H", "S", "V"]
+        labels = ["H", "S", "V"]
         nparr = cv.cvtColor(nparr, cv.COLOR_BGR2HSV)
     elif mode == "YCrCb":
-        mode = ["Y", "Cr", "Cb"]
+        labels = ["Y", "Cr", "Cb"]
         nparr = cv.cvtColor(nparr, cv.COLOR_BGR2YCrCb)
     else:
         raise Exception(f"{mode} not supported")
 
     for a, b in [(0, 1), (0, 2), (1, 2)]:
-        df = _hist2d_dataframe(nparr, a, b, 256)
+        df = _hist2d_dataframe(nparr, a, b, 256, labels)
         fig, ax = plt.subplots(figsize=(9, 6))
         sns.heatmap(df, cmap="jet", ax=ax)
+
+
+def color_hiplot(img_path, start=0, limit=1, mode="BGR", max_rows=10000):
+    # jupyter: %matplotlib inline
+    img_path = Path(img_path)
+
+    if img_path.is_file():
+        img_list = [img_path]
+    elif img_path.is_dir():
+        img_list = sorted(img_path.glob("**/*"))
+        img_list = [x for x in img_list if x.suffix in IMG_EXTENSIONS]
+    else:
+        raise TypeError(f"{img_path} is not a file or dir")
+
+    assert limit >= 1 and len(img_list) > start
+
+    img_path = img_list[start]
+    nparr = cv.imread(str(img_path), 1)
+
+    if mode == "BGR":
+        labels = ["B", "G", "R"]
+    elif mode == "HSV":
+        labels = ["H", "S", "V"]
+        nparr = cv.cvtColor(nparr, cv.COLOR_BGR2HSV)
+    elif mode == "YCrCb":
+        labels = ["Y", "Cr", "Cb"]
+        nparr = cv.cvtColor(nparr, cv.COLOR_BGR2YCrCb)
+    else:
+        raise Exception(f"{mode} not supported")
+
+    df = pd.DataFrame({label: nparr[..., i].ravel()
+                       for i, label in enumerate(labels)})
+
+    if df.shape[0] > max_rows:
+        df = df.sample(n=max_rows, random_state=100)
+
+    hip.Experiment.from_dataframe(df).display()
