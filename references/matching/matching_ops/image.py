91623f172c0e74e23769e61f0e0f0bea45e8f51a
hejian.host.mac <flystarhe@qq.com>
2021-05-22 19:39:34 +0800

    fine references/matching/matching_ops/

--- references/matching/matching_ops/image.py
+++ references/matching/matching_ops/image.py
@@ -36,32 +36,3 @@ def to_rgb(image, color="BGR", max_val=None):
 def crop_image(image, bbox):
     x1, y1, w, h = list(map(int, bbox))
     return image[y1:y1 + h, x1:x1 + w]
-
-
-def draw_bbox(image, bbox, color=(255, 0, 0)):
-    x1, y1, w, h = list(map(int, bbox))
-    pt1, pt2 = (x1, y1), (x1 + w, y1 + h)
-    return cv.rectangle(image, pt1, pt2, color)
-
-
-def show_image(image, color="RGB"):
-    from PIL import Image as Image2
-    from IPython.display import display
-
-    rgb = to_rgb(image, color=color)
-    display(Image2.fromarray(rgb, "RGB"))
-
-
-def show_images(images, cols=2, color="RGB"):
-    from PIL import Image as Image2
-    from IPython.display import display
-
-    images = [to_rgb(img, color=color) for img in images]
-
-    h_ = min([img.shape[0] for img in images])
-    w_ = min([img.shape[1] for img in images])
-    images = [img[:h_, :w_] for img in images]
-
-    for i in range(0, len(images), cols):
-        batch_rgb = np.concatenate(images[i:i + cols], axis=1)
-        display(Image2.fromarray(batch_rgb, "RGB"))
--- references/matching/matching_ops/visualize.py
+++ references/matching/matching_ops/visualize.py
@@ -3,9 +3,40 @@ import numpy as np
 from skimage.color import label2rgb
 from skimage.morphology import diamond
 
+from matching_ops.image import to_rgb
+
 
 def _color(mask, color):
     return np.stack([mask * c for c in color], axis=2)
+
+
+def draw_bbox(image, bbox, color=(255, 0, 0)):
+    x1, y1, w, h = list(map(int, bbox))
+    pt1, pt2 = (x1, y1), (x1 + w, y1 + h)
+    return cv.rectangle(image, pt1, pt2, color)
+
+
+def show_image(image, color="RGB"):
+    from IPython.display import display
+    from PIL import Image as Image2
+
+    rgb = to_rgb(image, color=color)
+    display(Image2.fromarray(rgb, "RGB"))
+
+
+def show_images(images, cols=2, color="RGB"):
+    from IPython.display import display
+    from PIL import Image as Image2
+
+    images = [to_rgb(img, color=color) for img in images]
+
+    h_ = min([img.shape[0] for img in images])
+    w_ = min([img.shape[1] for img in images])
+    images = [img[:h_, :w_] for img in images]
+
+    for i in range(0, len(images), cols):
+        batch_rgb = np.concatenate(images[i:i + cols], axis=1)
+        display(Image2.fromarray(batch_rgb, "RGB"))
 
 
 def label_regions(image, masks, colors=(255, 0, 0), only_edge=True):
