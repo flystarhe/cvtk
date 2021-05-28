be464ce57cf3f8eee5ca28271623f9d7e9a5813b
hejian.host.mac <flystarhe@qq.com>
2021-05-28 23:35:32 +0800

    post references/mmdetection/dev/tmpl_abc.py

---
+++ cvtk/text/utils.py
@@ -0,0 +1,27 @@
+import re
+
+
+def match(pattern, string):
+    # pattern like `r".*coco_file$"`
+    if pattern.match(string):
+        return True
+    return False
+
+
+def replace(obj, old, new):
+    if isinstance(obj, str):
+        return obj.replace(old, new)
+    return obj
+
+
+def replace2(pattern, data, old, new):
+    if isinstance(pattern, str):
+        pattern = re.compile(pattern)
+
+    def _func(k, v):
+        if match(pattern, k):
+            return replace(v, old, new)
+        return v
+
+    assert isinstance(data, dict), f"{type(data)} not supported"
+    return {k: _func(k, v) for k, v in data.items()}
---
+++ references/mmdetection/dev/tmpl_abc.py
@@ -0,0 +1,39 @@
+import copy
+from pathlib import Path
+from cvtk.text.utils import replace2
+from papermill import execute_notebook
+
+workdir = "/workspace/cvtk/references/mmdetection"
+outdir = Path("/workspace/notebooks/task_name")
+outdir.mkdir(parents=True, exist_ok=True)
+
+_cfg = dict(cfg_xlr=1.0,
+            cfg_times=3,
+            cfg_classes=[],
+            cfg_num_classes=20,
+            cfg_albu_p=0.5,
+            cfg_experiment_path='./tmp/ipynbname',
+            cfg_train_data_root='/workspace/datasets/xxxx',
+            cfg_train_coco_file='keep_p_samples/01/train.json',
+            cfg_val_data_root='/workspace/datasets/xxxx',
+            cfg_val_coco_file='keep_p_samples/01/test.json',
+            cfg_test_data_root='/workspace/datasets/xxxx',
+            cfg_test_coco_file='keep_p_samples/01/test.json')
+
+logs = []
+for input_path in ["test.ipynb"]:
+    for gid in [1, 2, 3]:
+        for xlr in [1.0, 2.0, 0.5]:
+            cfg = copy.deepcopy(_cfg)
+            input_path = Path(input_path)
+            fn = f"{input_path.stem}_{gid=:02d}_{xlr=:.1f}.ipynb"
+            cfg = replace2(r".*coco_file$", cfg, "/01/", f"/{gid:02d}/")
+            cfg.update(cfg_xlr=xlr, cfg_experiment_path=str(outdir / fn[:-6]))
+            log = execute_notebook(input_path, outdir / fn, parameters=cfg,
+                                   cwd=workdir, progress_bar=False,
+                                   kernel_name="python")
+            logs.append(log)
+
+nbs = " ".join([log["metadata"]["papermill"]["output_path"] for log in logs])
+# !jupyter nbconvert --to html {nbs}
+# !rm -rfv {nbs}
