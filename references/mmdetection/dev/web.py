351a56c80e719b3577ebb7cd3579b7a376a971de
hejian.host.mac <flystarhe@qq.com>
2021-06-03 10:04:36 +0800

    update references/mmdetection/dev/web.py - G_KW

--- references/mmdetection/dev/web.py
+++ references/mmdetection/dev/web.py
@@ -4,38 +4,71 @@ import traceback
 import tornado.ioloop
 import tornado.web
 from cvtk.io import imread
+from cvtk.utils.abc.gen import image_label
 from mmdet.datasets.pipelines import Compose
 
 from py_app import inference_detector, init_detector
+
+G_KW = dict(mode="max_score", score_thr={"*": 0.3}, label_grade={"*": 1})
+G_MODEL = None
+G_DEVICE = None
+G_PIPLINE = None
 
 
 class MainHandler(tornado.web.RequestHandler):
 
     def post(self):
         try:
-            global model, device, test_pipeline
-            imgs = [imread(f, 1) for f in self.get_arguments("image")]
-            output = inference_detector(imgs, model, device, test_pipeline)
+            global G_KW, G_MODEL, G_DEVICE, G_PIPLINE
+
+            file_list = self.get_arguments("image")
+            imgs = [imread(uri, 1) for uri in file_list]
+            results = inference_detector(imgs, G_MODEL, G_DEVICE, G_PIPLINE)
+            labels = [image_label(dts, **G_KW) for dts in results]
+            output = list(zip(file_list, labels, results))
+
             self.finish({"status": 0, "data": output})
         except Exception:
             output = traceback.format_exc()
             self.finish({"status": 1, "data": output})
 
 
-# CUDA_VISIBLE_DEVICES=1 python app_tornado.py 7000
+class SettingsHandler(tornado.web.RequestHandler):
+
+    def post(self):
+        try:
+            global G_KW
+
+            mode = self.get_argument("mode", None)
+            if mode is not None:
+                G_KW["mode"] = mode
+
+            for k in ["score_thr", "label_grade"]:
+                v = self.get_argument(k, None)
+                if v is not None:
+                    G_KW[k] = eval(v)
+
+            self.finish({"status": 0, "data": G_KW})
+        except Exception:
+            output = traceback.format_exc()
+            self.finish({"status": 1, "data": output})
+
+
+# CUDA_VISIBLE_DEVICES=1 python web.py 7000 config checkpoint
 if __name__ == "__main__":
     args = sys.argv[1:]
 
     port = int(args[0])
-    model = init_detector(args[1], args[2], device="cuda:0")
-    device = next(model.parameters()).device
+    G_MODEL = init_detector(args[1], args[2], device="cuda:0")
+    G_DEVICE = next(G_MODEL.parameters()).device
 
-    cfg = model.cfg.copy()
-    test_pipeline = cfg.data.test.pipeline
-    test_pipeline[0].type = "LoadImageFromWebcam"
-    test_pipeline = Compose(test_pipeline)
+    cfg = G_MODEL.cfg.copy()
+    G_PIPLINE = cfg.data.test.pipeline
+    G_PIPLINE[0].type = "LoadImageFromWebcam"
+    G_PIPLINE = Compose(G_PIPLINE)
 
     tornado.web.Application([
         (r"/predict", MainHandler),
+        (r"/settings", SettingsHandler),
     ]).listen(port)
     tornado.ioloop.IOLoop.current().start()
