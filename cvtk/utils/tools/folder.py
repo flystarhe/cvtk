699d172c454c309c74efff7d5eac04d8aa4794f2
hejian.host.mac <flystarhe@qq.com>
2021-06-09 18:12:37 +0800

    post cvtk/utils/tools/folder.py - split_folder

---
+++ cvtk/utils/tools/folder.py
@@ -0,0 +1,53 @@
+import shutil
+import pandas as pd
+from pathlib import Path
+
+
+def _save_to(f, indir, outdir, level=None):
+    if level is None:
+        return outdir / f.name
+
+    f = f.relative_to(indir)
+    return outdir / Path(*f.parts[-level:])
+
+
+def split_folder(indir, outdir, ref_file, col_path, col_label, suffixes=".jpg", level=1):
+    indir = Path(indir)
+
+    if outdir is None:
+        outdir = f"{str(indir)}_TMP"
+
+    outdir = Path(outdir)
+    shutil.rmtree(outdir, ignore_errors=True)
+
+    data = list(indir.glob("**/*.*"))
+    data = {f.name: f for f in sorted(data)}
+
+    src_list = list(data.values())
+
+    if isinstance(suffixes, str):
+        suffixes = set(suffixes.split(","))
+        src_list = [f for f in src_list if f.suffix in suffixes]
+
+    if ref_file is not None:
+        if ref_file.endswith(".csv"):
+            df = pd.read_csv(ref_file)
+        elif ref_file.endswith(".xlsx"):
+            df = pd.read_excel(ref_file, "Sheet1")
+        else:
+            raise Exception(f"{ref_file} not supported")
+
+        mapping = {Path(row[col_path]).name: row[col_label]
+                   for _, row in df.itertuples()}
+        dst_list = [_save_to(f, indir, outdir / mapping[f.name])
+                    for f in src_list]
+    else:
+        dst_list = [_save_to(f, indir, outdir, level=level) for f in src_list]
+
+    for f in sorted(set([f.parent for f in dst_list])):
+        f.mkdir(parents=True, exist_ok=True)
+
+    for src, dst in zip(src_list, dst_list):
+        shutil.copyfile(src, dst)
+
+    return f"copy {len(src_list)} file to {outdir}"
