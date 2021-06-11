35806a9282df010265675a05546b82275694569b
hejian.host.mac <flystarhe@qq.com>
2021-06-11 09:25:18 +0800

    fix references/mmdetection/dev/tmpl_abc.py

--- references/mmdetection/dev/tmpl_abc.py
+++ references/mmdetection/dev/tmpl_abc.py
@@ -3,9 +3,9 @@ from cvtk.utils import notebook
 cfg = dict(
     cfg_times=3,
     cfg_lr=0.02/4,
-    cfg_classes=[],
-    cfg_num_classes=20,
-    cfg_albu_p=0.5,
+    cfg_classes=[],  # must modify
+    cfg_num_classes=20,  # must modify
+    cfg_albu_p=0.5,  # must modify
     cfg_num_gpus=2,
     cfg_mini_batch=2,
     cfg_multi_scale=[],
@@ -13,12 +13,12 @@ cfg = dict(
     cfg_load_from=None,
     cfg_frozen_stages=1,
     cfg_experiment_path='./tmp/ipynbname',
-    cfg_train_data_root='/workspace/notebooks/xxxx',
-    cfg_train_coco_file='keep_p_samples/01/train.json',
-    cfg_val_data_root='/workspace/notebooks/xxxx',
-    cfg_val_coco_file='keep_p_samples/01/test.json',
-    cfg_test_data_root='/workspace/notebooks/xxxx',
-    cfg_test_coco_file='keep_p_samples/01/test.json',
+    cfg_train_data_root='/workspace/notebooks/xxxx',  # must modify
+    cfg_train_coco_file='keep_p_samples/01/train.json',  # must modify
+    cfg_val_data_root='/workspace/notebooks/xxxx',  # must modify
+    cfg_val_coco_file='keep_p_samples/01/test.json',  # must modify
+    cfg_test_data_root='/workspace/notebooks/xxxx',  # must modify
+    cfg_test_coco_file='keep_p_samples/01/test.json',  # must modify
     cfg_tmpl_path='/usr/src/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py',
 )
 
@@ -27,8 +27,18 @@ outdir = cfg['cfg_train_data_root'] + '_T0000'
 # !rm -rf {outdir}
 
 task_list = [
-    dict(cfg_times=2, cfg_lr=0.02/16*4, group_id=1, input_path=''),
-    dict(cfg_times=1, cfg_lr=0.02/16*4/25, group_id=1, input_path=''),
+    dict(cfg_times=2, cfg_lr=0.02/16*4, cfg_num_gpus=2, cfg_mini_batch=2,
+         cfg_multi_scale=[], cfg_crop_size=800, group_id=1,
+         cfg_train_coco_file='keep_p_samples/01/train.json',
+         cfg_test_coco_file='keep_p_samples/01/test.json',
+         cfg_val_coco_file='keep_p_samples/01/test.json',
+         input_path='/workspace/cvtk/references/mmdetection/dev/tmpl_s1_cos.ipynb'),
+    dict(cfg_times=1, cfg_lr=0.02/16*4/25, cfg_num_gpus=2, cfg_mini_batch=2,
+         cfg_multi_scale=[], cfg_crop_size=0, group_id=1,
+         cfg_train_coco_file='keep_p_samples/01/train.json',
+         cfg_test_coco_file='keep_p_samples/01/test.json',
+         cfg_val_coco_file='keep_p_samples/01/test.json',
+         input_path='/workspace/cvtk/references/mmdetection/dev/tmpl_s1_cos.ipynb'),
 ]
 carry_on = None  # None, 'latest', ['key-name']
 res1, cached1 = notebook.run(task_list, carry_on, workdir, outdir, cfg)
