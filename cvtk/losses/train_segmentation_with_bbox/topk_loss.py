c0964ba95b41af68235873a86b3226c1f1d6a70b
hejian.host.mac <flystarhe@qq.com>
2021-07-16 13:11:04 +0800

    update cvtk/losses/train_segmentation_with_bbox/topk_loss.py#topk:7->(a+b)

--- cvtk/losses/train_segmentation_with_bbox/topk_loss.py
+++ cvtk/losses/train_segmentation_with_bbox/topk_loss.py
@@ -30,7 +30,7 @@ def make_target(s, feats, bboxes, labels=None, topk=3, balance=False, use_sigmoi
             continue
 
         sub_target = torch.full_like(roi, -100, dtype=torch.int64)
-        k = topk if topk is not None else 7  # max(roi.shape)
+        k = topk or sum(roi.shape)  # 7 or max(roi.shape)
         selected = _mask_top_by_full(roi, k, eps)
         sub_target[selected] = label
 
